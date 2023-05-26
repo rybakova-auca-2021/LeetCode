081600bd4c5b2522ff460a08455ebc32b8752abd
Kamila Rybakova <rybakova_k@auca.kg>
2023-05-26 17:04:23 +0200

    Two Sum completed

---
+++ LeetCode1.c
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i + 1; j < nums.size(); j++) {
+                int x = nums[i];
+                int y = nums[j];
+                if(x + y == target) {
+                    return {i, j};
+                }
+            }
+        } 
+        return {};  
+    }
+};
