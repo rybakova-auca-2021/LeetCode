b689e3535ec11149e2916dfa5adc843abac86b4b
tybakova-auca-2021 <rybakova_k@auca.kg>
2023-06-21 21:03:04 +0200

    Plus One

---
+++ LeetCode66.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& v) {
+        for(int i = v.size() - 1; i >= 0; i--) {
+            if(v[i] < 9) {
+                v[i]++;
+                return v;
+            } else {
+                v[i] = 0;
+            }
+        }
+        v.push_back(0);
+        v[0] = 1;
+        return v;
+    }
+};
