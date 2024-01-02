5df89ba5f93940c3df071b4b3e779e038d98ecc7
rybakova-auca-2021 <rybakova_k@auca.kg>
2024-01-02 23:14:12 +0600

    LeetCode 219 solved

---
+++ LeetCode219.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        int size = nums.size();
+
+        for(int i = 0; i < size; i++) {
+            if(map.count(nums[i])) {
+                if(abs(i - map[nums[i]]) <= k) {
+                    return true;
+                }
+            }
+            map[nums[i]] = i;
+        }
+        return false;
+    }
+};
